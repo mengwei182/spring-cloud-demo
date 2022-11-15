1f5406d3685f57f1a538ad48c93e2f9e24287830
mengwei182 <mengwei182@outlook.com>
2022-11-15 15:14:26 +0800

    提交文件

---
+++ common-server/src/main/java/org/example/common/configuration/RedisConfiguration.java
@@ -0,0 +1,34 @@
+package org.example.common.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author 李辉
+ * @since 2022/11/15
+ */
+@Configuration
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        // String的序列化方式
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // Jackson2JsonRedisSerializer序列化类
+        Jackson2JsonRedisSerializer<?> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        // key采用String的序列化方式
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的key也采用String的序列化方式
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        //hash的value也采用jackson
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        return redisTemplate;
+    }
+}
