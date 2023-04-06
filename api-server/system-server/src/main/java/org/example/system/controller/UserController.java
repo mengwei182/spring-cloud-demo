c73c77646382231d9a5e1c0be49b3c5aa403739f
mengwei182 <mengwei182@outlook.com>
2023-04-06 22:31:54 +0800

    提交文件

--- api-server/system-server/src/main/java/org/example/system/controller/UserController.java
+++ api-server/system-server/src/main/java/org/example/system/controller/UserController.java
@@ -19,51 +19,97 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    /**
+     * 登录
+     *
+     * @param usernamePasswordVo
+     * @return
+     */
     @RequestMapping("/login")
     public CommonResult login(@Valid @RequestBody UsernamePasswordVo usernamePasswordVo) {
         return CommonResult.success(userService.login(usernamePasswordVo));
     }
 
+    /**
+     * 登出
+     *
+     * @return
+     */
     @RequestMapping("/logout")
     public CommonResult logout() {
         return CommonResult.success(userService.logout());
     }
 
+    /**
+     * 注册
+     *
+     * @param userInfoVo
+     * @return
+     */
     @RequestMapping("/register")
     public CommonResult register(@Valid @RequestBody UserInfoVo userInfoVo) {
         return CommonResult.success(userService.register(userInfoVo));
     }
 
+    /**
+     * 图片验证码
+     *
+     * @param response
+     * @throws IOException
+     */
     @RequestMapping("/image/verify/code")
     public void getImageVerifyCode(HttpServletResponse response) throws IOException {
         userService.generateImageVerifyCode(response);
     }
 
+    /**
+     * 分页查看用户列表
+     *
+     * @param queryPage
+     * @return
+     */
     @RequestMapping("/user/list")
     public CommonResult getUserList(@ModelAttribute UserQueryPage queryPage) {
         return CommonResult.success(userService.getUserList(queryPage));
     }
 
+    /**
+     * 查看用户详情
+     *
+     * @return
+     */
     @RequestMapping("/user/info")
     public CommonResult getUserInfo() {
         return CommonResult.success(userService.getUserInfo(UserContext.get().getUserId()));
     }
 
-    @RequestMapping("/user/edit")
+    /**
+     * 更新用户信息
+     *
+     * @param userInfoVo
+     * @return
+     */
+    @RequestMapping("/user/update")
     public CommonResult updateUser(@RequestBody UserInfoVo userInfoVo) {
         return CommonResult.success(userService.updateUser(userInfoVo));
     }
 
+    /**
+     * 更新密码
+     *
+     * @param usernamePasswordVo
+     * @return
+     */
     @RequestMapping("/user/updatePassword")
     public CommonResult updateUserPassword(@Valid @RequestBody UsernamePasswordVo usernamePasswordVo) {
         return CommonResult.success(userService.updateUserPassword(usernamePasswordVo));
     }
 
-    @RequestMapping("/user/updateRole")
-    public CommonResult updateUserRole(@RequestBody UserRoleRelationVo userRoleRelationVo) {
-        return CommonResult.success(userService.updateUserRole(userRoleRelationVo));
-    }
-
+    /**
+     * 删除用户
+     * @param id
+     * @return
+     */
     @RequestMapping("/user/delete")
     public CommonResult deleteUser(@RequestParam String id) {
         return CommonResult.success(userService.deleteUser(id));
--- common-server/src/main/java/org/example/common/entity/vo/UserInfoVo.java
+++ common-server/src/main/java/org/example/common/entity/vo/UserInfoVo.java
@@ -14,26 +14,42 @@ import java.util.Date;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class UserInfoVo extends BaseEntity {
-    // 用户名
+    /**
+     * 用户名
+     */
     @NotNull(message = "用户名不能为空")
     private String username;
-    // 密码
+    /**
+     * 密码
+     */
     @NotNull(message = "密码不能为空")
     private String password;
-    // 头像
+    /**
+     * 头像
+     */
     private String icon;
-    // 手机号
+    /**
+     * 手机号
+     */
     private String phone;
-    // 邮箱
+    /**
+     * 邮箱
+     */
     private String email;
-    // 姓名
+    /**
+     * 姓名
+     */
     private String name;
     /**
      * 描述
      */
     private String description;
-    // 登录时间
+    /**
+     * 登录时间
+     */
     private Date loginTime;
-    // 0禁用，1正常，2需要手机验证码，3需要图形验证码，4需要手机验证码和图像验证码
+    /**
+     * 0禁用，1正常
+     */
     private Integer status;
 }
