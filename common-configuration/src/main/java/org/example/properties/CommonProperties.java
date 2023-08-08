b185232225012227f75e924400e73825f8d184d4
lihui <lihui@3h1china.com>
2023-08-08 10:36:35 +0800

    提交文件

--- common-configuration/src/main/java/org/example/properties/CommonProperties.java
+++ common-configuration/src/main/java/org/example/properties/CommonProperties.java
@@ -1,18 +1,16 @@
 package org.example.properties;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * @author lihui
  * @since 2023/4/3
  */
 @Data
-@Configuration
-@PropertySource("classpath:common.properties")
+@Component
+@ConfigurationProperties(prefix = "common")
 public class CommonProperties {
-    @Value("${url.white.list}")
-    private String urlWhiteList;
+    private String skipUrl;
 }
